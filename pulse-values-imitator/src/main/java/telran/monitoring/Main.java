696e8e23a888ed599cbecc0ba68a143eb71bb869
Yuragranovsky <yuragranovsky@gmail.com>
2025-03-04 11:31:10 +0200

    pulse imitator / receiver

--- pulse-values-imitator/src/main/java/telran/monitoring/Main.java
+++ pulse-values-imitator/src/main/java/telran/monitoring/Main.java
@@ -4,6 +4,7 @@ import telran.monitoring.api.SensorData;
 import telran.monitoring.logging.Logger;
 import telran.monitoring.logging.LoggerStandard;
 import java.net.*;
+import java.util.HashMap;
 import java.util.Random;
 import java.util.stream.IntStream;
 
@@ -16,7 +17,13 @@ public class Main {
     static final String DEFAULT_HOST = "localhost";
     static final int DEFAULT_PORT = 5000;
     static final int DEFAULT_N_PATIENTS = 10;
-    static final int DEFAULT_N_PACKETS = 50;
+    static final int DEFAULT_N_PACKETS = 1000;
+    static final int JUMP_PROB = 10;
+    static final int MIN_JUMP_PERCENT = 10;
+    static final int MAX_JUMP_PERCENT = 100;
+    static final int JUMP_POSITIVE_PROB = 70;
+    static final long PATIENT_ID_FOR_INFO_LOGGING = 3;
+    static HashMap<Long, Integer> patientIdPulseValue = new HashMap<>();
     static DatagramSocket socket = null;
 
     public static void main(String[] args) throws Exception {
@@ -27,36 +34,70 @@ public class Main {
 
     static void send(int i) {
         SensorData sensor = getRandomSensorData(i);
+        logger.log("finest", sensor.toString());
+        if (sensor.patientId() == PATIENT_ID_FOR_INFO_LOGGING) {
+            logger.log("info", String.format("Pulse value for patient %d is %d",
+                    PATIENT_ID_FOR_INFO_LOGGING, sensor.value()));
+        }
         String jsonStr = sensor.toString();
         try {
             udpSend(jsonStr);
         } catch (Exception e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
 
-    private static void udpSend(String jsonStr) throws Exception{
+    static void udpSend(String jsonStr) throws Exception {
         logger.log("finest", String.format("data to be sent is %s", jsonStr));
         byte[] bufferSend = jsonStr.getBytes();
         DatagramPacket packet = new DatagramPacket(bufferSend, bufferSend.length,
-         InetAddress.getByName(DEFAULT_HOST), DEFAULT_PORT);
-         socket.send(packet);
-         socket.receive(packet);
-         if(!jsonStr.equals(new String(packet.getData()))) {
-                throw new Exception("received packet doesn't equal the sent one");
-         }
+                InetAddress.getByName(DEFAULT_HOST), DEFAULT_PORT);
+        socket.send(packet);
+        socket.receive(packet);
+        if (!jsonStr.equals(new String(packet.getData()))) {
+            throw new Exception("received packet doesn't equal the sent one");
+        }
     }
 
-    private static SensorData getRandomSensorData(int i) {
+    static SensorData getRandomSensorData(int i) {
         long patientId = getRandomNumber(1, DEFAULT_N_PATIENTS);
-        int value = getRandomNumber(MIN_PULSE_VALUE, MAX_PULSE_VALUE);
+        int value = getRandomPulseValue(patientId);
+        ;
         long timestamp = System.currentTimeMillis();
         SensorData res = new SensorData(patientId, value, timestamp);
         return res;
     }
 
-    private static int getRandomNumber(int minValue, int maxValue) {
+    private static int getRandomPulseValue(long patientId) {
+        int valueRes = patientIdPulseValue.computeIfAbsent(patientId,
+				k -> getRandomNumber(MIN_PULSE_VALUE, MAX_PULSE_VALUE));
+		if (chance(JUMP_PROB)) {
+			valueRes = getValueWithJump(valueRes);
+			patientIdPulseValue.put(patientId, valueRes);
+		}
+
+		return valueRes;
+    }
+
+    static int getRandomNumber(int minValue, int maxValue) {
         return new Random().nextInt(minValue, maxValue + 1);
     }
+    private static boolean chance(int prob) {
+
+		return getRandomNumber(0, 99) < prob;
+	}
+    private static int getValueWithJump(int previousValue) {
+		int jumpPercent = getRandomNumber(MIN_JUMP_PERCENT, MAX_JUMP_PERCENT);
+		int jumpValue = previousValue * jumpPercent / 100;
+		if (!chance(JUMP_POSITIVE_PROB)) {
+			jumpValue = -jumpValue;
+		}
+		int res = previousValue + jumpValue;
+		if (res < MIN_PULSE_VALUE) {
+			res = MIN_PULSE_VALUE;
+		} else if (res > MAX_PULSE_VALUE) {
+			res = MAX_PULSE_VALUE;
+		}
+		return res;
+	}
 }
--- pulse-values-receiver/src/main/java/telran/monitoring/Main.java
+++ pulse-values-receiver/src/main/java/telran/monitoring/Main.java
@@ -2,23 +2,47 @@ package telran.monitoring;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.SocketAddress;
+
+import telran.monitoring.api.SensorData;
+import telran.monitoring.logging.Logger;
+import telran.monitoring.logging.LoggerStandard;
 
 public class Main {
 
     private static final int PORT = 5000;
     private static final int MAX_SIZE = 1500;
+    private static final int WARNING_LOG_VALUE = 220;
+    private static final int ERROR_LOG_VALUE = 230;
+    static Logger logger = new LoggerStandard("receiver");
 
     public static void main(String[] args) throws Exception {
         DatagramSocket socket = new DatagramSocket(PORT);
+
         byte[] buffer = new byte[MAX_SIZE];
         while (true) {
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
             socket.receive(packet);
-            System.out.println(new String(packet.getData()));
-            
+            String jsonStr = new String(packet.getData());
+            logger.log("finest", jsonStr);
+            logPulseValue(jsonStr);
+
             socket.send(packet);
         }
+    }
+
+    private static void logPulseValue(String jsonStr) {
+        SensorData sensorData = SensorData.of(jsonStr);
+        int value = sensorData.value();
+        if (value >= WARNING_LOG_VALUE && value <= ERROR_LOG_VALUE) {
+            logValue("warning", sensorData);
+        } else if(value > ERROR_LOG_VALUE) {
+            logValue("error", sensorData);
+        }
+    }
+
+    private static void logValue(String level, SensorData sensorData) {
+        logger.log(level, String.format("patient %d has pulse value greater than %d", sensorData.patientId(),
+                level.equals("warning") ? WARNING_LOG_VALUE : ERROR_LOG_VALUE));
     }
 
 }
