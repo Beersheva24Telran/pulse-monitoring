680256ed443ec8b82913229ab9bdbdb8bab7c623
Yuragranovsky <yuragranovsky@gmail.com>
2025-03-05 14:01:33 +0200

    introducing abstractDataStream

--- pulse-values-receiver/src/main/java/telran/monitoring/Main.java
+++ pulse-values-receiver/src/main/java/telran/monitoring/Main.java
@@ -2,6 +2,7 @@ package telran.monitoring;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.util.Map;
 
 import telran.monitoring.api.SensorData;
 import telran.monitoring.logging.Logger;
@@ -13,29 +14,48 @@ public class Main {
     private static final int MAX_SIZE = 1500;
     private static final int WARNING_LOG_VALUE = 220;
     private static final int ERROR_LOG_VALUE = 230;
-    static Logger logger = new LoggerStandard("receiver");
+    private static final String DEFAULT_PULSE_VALUES_STREAM = "pulse_values";
+        private static final String DEFAULT_STREAM_CLASS_NAME = "telran.monitoring.DynamoDbStreamSensorData";
+        static Logger logger = new LoggerStandard("receiver");
+        static Map<String, String> env = System.getenv();
+    
+        public static void main(String[] args) {
+    
+            try (DatagramSocket socket = new DatagramSocket(PORT);) {
+                @SuppressWarnings("unchecked")
+                MiddlewareDataStream<SensorData> stream = MiddlewareDataStreamFactory.getStream(getDataStreamClassName(), getTableName());
+                byte[] buffer = new byte[MAX_SIZE];
+                while (true) {
+                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                    socket.receive(packet);
+                    String jsonStr = new String(packet.getData());
+                    logPulseValue(jsonStr);
+    
+                    socket.send(packet);
+                    stream.publish(SensorData.of(jsonStr));
 
-    public static void main(String[] args) throws Exception {
-        DatagramSocket socket = new DatagramSocket(PORT);
-
-        byte[] buffer = new byte[MAX_SIZE];
-        while (true) {
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-            socket.receive(packet);
-            String jsonStr = new String(packet.getData());
-            logger.log("finest", jsonStr);
-            logPulseValue(jsonStr);
-
-            socket.send(packet);
+                }
+            } catch (Exception e) {
+                logger.log("severe", e.toString());
+            }
+    
         }
+    
+        private static String getTableName() {
+            return env.getOrDefault("STREAM_NAME", DEFAULT_PULSE_VALUES_STREAM);
+        }
+    
+        private static String getDataStreamClassName() {
+            return env.getOrDefault("DATA_STREAM_CLASS_NAME", DEFAULT_STREAM_CLASS_NAME);
     }
 
     private static void logPulseValue(String jsonStr) {
+        logger.log("finest", jsonStr);
         SensorData sensorData = SensorData.of(jsonStr);
         int value = sensorData.value();
         if (value >= WARNING_LOG_VALUE && value <= ERROR_LOG_VALUE) {
             logValue("warning", sensorData);
-        } else if(value > ERROR_LOG_VALUE) {
+        } else if (value > ERROR_LOG_VALUE) {
             logValue("error", sensorData);
         }
     }
