ab143c65b1e7941a272c38d2a4c007baa87eaee9
Yuragranovsky <yuragranovsky@gmail.com>
2025-03-28 15:40:22 +0300

    adds logs in MailSender interface

--- sender-mails/src/main/java/telran/monitoring/MailSender.java
+++ sender-mails/src/main/java/telran/monitoring/MailSender.java
@@ -1,13 +1,22 @@
 package telran.monitoring;
+import java.lang.reflect.Constructor;
+
 import telran.monitoring.logging.*;
 public interface MailSender {
     static Logger [] loggers = new Logger[1];
     void sendMail(String subject, String recipientAddress, String text);
     static MailSender getMailSender(String mailSenderClassName, Logger logger) {
         loggers[0] = logger;
-        loggers[0].log("finest", "class name from MailSender interface is " + mailSenderClassName);
+        logger.log("finest", "class name from MailSender interface is " + mailSenderClassName);
         try {
-            return (MailSender)Class.forName(mailSenderClassName).getConstructor().newInstance();
+            @SuppressWarnings("unchecked")
+            Class<MailSender> clazz = (Class<MailSender>) Class.forName(mailSenderClassName);
+            logger.log("finest", "class in MailSender created");
+            Constructor<MailSender> constructor = clazz.getConstructor();
+            logger.log("finest", "constructor in MailSender created");
+            MailSender instance = constructor.newInstance();
+            logger.log("finest", "instance in MailSender created");
+            return instance;
         } catch(Exception e) {
 
             throw new RuntimeException(e);
