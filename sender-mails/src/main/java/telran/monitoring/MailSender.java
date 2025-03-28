441e6abd799cb5aeccbf9e330c5f1ac2ec93d9d2
Yuragranovsky <yuragranovsky@gmail.com>
2025-03-28 14:59:27 +0300

    adds log in MailSender

--- sender-mails/src/main/java/telran/monitoring/MailSender.java
+++ sender-mails/src/main/java/telran/monitoring/MailSender.java
@@ -5,6 +5,7 @@ public interface MailSender {
     void sendMail(String subject, String recipientAddress, String text);
     static MailSender getMailSender(String mailSenderClassName, Logger logger) {
         loggers[0] = logger;
+        loggers[0].log("finest", "class name from MailSender interface is " + mailSenderClassName);
         try {
             return (MailSender)Class.forName(mailSenderClassName).getConstructor().newInstance();
         } catch(Exception e) {
